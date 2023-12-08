722b6963d3d359bf4ff42e795f67b488367839d6
maya <maiia.ovsiannikova@gmail.com>
2023-12-08 15:11:51 +0300

    clear commit

--- requests.sql
+++ requests.sql
@@ -7,13 +7,43 @@ set search_path = rental_service;
 
 -- создание таблиц
 drop table if exists client cascade;
-create table client (
-    client_id integer primary key,
-    address_id integer not null,
-    name varchar(50) not null,
-    surname varchar(50),
-    birthday date,
-    phone varchar(20)
+create table client
+(
+    client_id  integer primary key,
+    address_id integer     not null,
+    name       varchar(50) not null,
+    surname    varchar(50),
+    birthday   date,
+    phone      varchar(20) unique
 );
+
+drop table if exists service cascade;
+create table service
+(
+    service_id integer primary key,
+    name       varchar(50) not null,
+    price      integer     not null
+);
+
+drop table if exists occasion cascade;
+create table occasion
+(
+    occasion_id integer primary key,
+    client_id   integer   not null,
+    datetime    timestamp not null
+);
+
+drop table if exists promotion cascade;
+create table promotion
+(
+    promotion_id      integer primary key,
+    client_id         integer not null,
+    service_id        integer not null,
+    promotion_type_id integer not null,
+    begin_t           timestamp,
+    end_t             timestamp,
+    promotion_name    text
+);
+
 
 -- task 4
