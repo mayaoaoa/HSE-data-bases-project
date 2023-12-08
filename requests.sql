ee1787af99787e9643ca41c62f7e4140e2bd067b
maya <maiia.ovsiannikova@gmail.com>
2023-12-08 14:44:51 +0300

    add client table

--- requests.sql
+++ requests.sql
@@ -5,4 +5,15 @@ create schema rental_service;
 
 set search_path = rental_service;
 
+-- создание таблиц
+drop table if exists client cascade;
+create table client (
+    client_id integer primary key,
+    address_id integer not null,
+    name varchar(50) not null,
+    surname varchar(50),
+    birthday date,
+    phone varchar(20)
+);
+
 -- task 4
