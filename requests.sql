6312bb111ebce0d001be09e972468ba3a2fe8064
lizzinni <l9273143@gmail.com>
2023-12-08 17:48:37 +0300

    task 4 (client, promotion, promotion_type)

--- requests.sql
+++ requests.sql
@@ -108,6 +108,9 @@ create table client_x_promotion
 );
 
 -- task 4
+
+set datastyle = 'DMY';
+
 insert into rental_service.address(address_id, address_name)
 VALUES (1, '3-я улица Строителей, дом 25'),
        (2, 'аллея Смурфиков, дом 3'),
@@ -123,3 +126,48 @@ VALUES (1, 'Коньки "Жизнь на острие"', 500),
        (5, 'Сноуборд "Полет нормальный"', 1000),
        (6, 'Скандинавские палочки для ходьби "Попу не отбей!"', 200),
        (7, 'Санки и финские сани "Любишь кататься -- люби и саночки возить!"', 400);
+
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('1', '1', 'Маша', 'Сергеева', '12.12.2012', '89213659022');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('2', '1', 'Василиса', 'Васильева', '05.10.1998', '89280923677');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('3', '1', 'Петя', 'Дмитриев', '09.03.1978', '89229002277');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('4', '2', 'Степанида', 'Дубровская', '29.08.2000', '89134657188');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('5', '3', 'Василиса', 'Горькая', '20.09.1950', '89026337890');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('6', '3', 'Вася', 'Пупкин', '10.02.1980', '89290234675');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('7', '3', 'Акакий', 'Сергеев', '16.10.2010', '89220007861');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('8', '4', 'Наташа', 'Ростова', '21.06.1975', '89219017833');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('9', '4', 'Александр', 'Дюма', '31.01.1965', '89219200471');
+insert into rental_service.client (client_id, address_id, name, surname, birthday, phone)
+    values ('10', '5', 'Федя', 'Шишкин', '11.11.11', '89239018821');
+
+
+insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
+    values ('1', '1', '10.12.2022', '01.01.2023', 'Не плати за третьи коньки');
+insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
+    values ('2', '1', '15.01.2022', '15.02.2022', 'Взяли в аренду лыжи и коньки? Аренда ватрушки в подарок');
+insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
+    values ('3', '2', '01.12.2022', '28.02.2023', 'Посетителям старше 60 лет скидка на прокат палок для скандинавской ходьбы');
+insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
+    values ('4', '2', '01.12.2022', '28.02.2023', 'Детям прокат санок и ватрушек делешевле на 20%');
+insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
+    values ('5', '4', '01.01.2023', '31.01.2023', 'Скидка 35% на аренду любых лыж');
+insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
+    values ('6', '3', '01.02.2023', '31.02.2023', 'Кто рано встает, тому сноуборд со скидкой 10% - успей оформить аренду до 10:00 и будет тебе счастье');
+
+
+insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
+    values ('1', '3 по цене 2');
+insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
+    values ('2', 'Скидка по возрасту');
+insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
+    values ('3', 'Скидка по времени');
+insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
+    values ('4', 'Скидка k%');
