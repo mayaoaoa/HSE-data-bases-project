c9bdb3754903aeb87200e7d23203f09261ee2a3f
maya <maiia.ovsiannikova@gmail.com>
2023-12-08 16:09:45 +0300

    task 3 without references

--- requests.sql
+++ requests.sql
@@ -5,7 +5,7 @@ create schema rental_service;
 
 set search_path = rental_service;
 
--- создание таблиц
+-- создание таблиц todo all foreign keys
 drop table if exists client cascade;
 create table client
 (
@@ -21,7 +21,7 @@ drop table if exists service cascade;
 create table service
 (
     service_id integer primary key,
-    name       varchar(50) not null,
+    naming     varchar(50) not null,
     price      integer     not null
 );
 
@@ -42,8 +42,49 @@ create table promotion
     promotion_type_id integer not null,
     begin_t           timestamp,
     end_t             timestamp,
-    promotion_name    text
+    promotion_name    text    not null
 );
 
+drop table if exists address cascade;
+create table address
+(
+    address_id   integer primary key,
+    address_name varchar(50) not null
+);
+
+drop table if exists promotion_type cascade;
+create table promotion_type
+(
+    promotion_type_id   integer primary key,
+    promotion_type_name text not null
+);
+
+drop table if exists occasion_x_service cascade;
+create table occasion_x_service
+(
+    occasion_id integer, --todo
+    service_id  integer  --todo
+);
+
+drop table if exists address_x_promotion_type cascade;
+create table address_x_promotion_type
+(
+    address_id        integer, --todo
+    promotion_type_id integer  --todo
+);
+
+drop table if exists service_x_promotion_type cascade;
+create table service_x_promotion_type
+(
+    service_id        integer, --todo
+    promotion_type_id integer  --todo
+);
+
+drop table if exists client_x_promotion cascade;
+create table client_x_promotion
+(
+    client_id    integer, --todo
+    promotion_id integer  --todo
+);
 
 -- task 4
