0631f9c722622c1202881e78446d7c339e51a203
lizzinni <l9273143@gmail.com>
2023-12-19 00:10:42 +0300

    task 4 coach

--- requests.sql
+++ requests.sql
@@ -165,14 +165,21 @@ insert into rental_service.client (client_id, address_id, name, surname, birthda
 values ('10', '5', 'Федя', 'Шишкин', '11.11.11', '89239018821');
 
 
-insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
-values ('1', '3 по цене 2');
-insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
-values ('2', 'Скидка по возрасту');
-insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
-values ('3', 'Скидка по времени');
-insert into rental_service.promotion_type (promotion_type_id, promotion_type_name)
-values ('4', 'Скидка k%');
+insert into rental_service.coach (coach_id, coach_name, experience, phone)
+values ('1', 'Степанида Васильева', '3', '+79234160751');
+insert into rental_service.coach (coach_id, coach_name, experience, phone)
+values ('2', 'Агрофена Чебурашкина', '10', '+79213416722');
+insert into rental_service.coach (coach_id, coach_name, experience, phone)
+values ('3', 'Геннадий Полотенцев', '6', '82222222222');
+insert into rental_service.coach (coach_id, coach_name, experience, phone)
+values ('4', 'Василий Грибников-Белкин', '2', '81923044567');
+insert into rental_service.coach (coach_id, coach_name, experience, phone)
+values ('5', 'Корней Копатычев', '14', '+78112460999');
+insert into rental_service.coach (coach_id, coach_name, experience, phone)
+values ('6', 'Капитолина Диванова', '7', '89009127741');
+insert into rental_service.coach(coach_id, coach_name, experience, phone)
+values ('7', 'Прасковья Шоколадкина', '3', '+7925617891');
+
 
 insert into rental_service.promotion (promotion_id, promotion_type_id, begin_t, end_t, promotion_name)
 values ('1', '1', '10.12.2022', '01.01.2023', 'Не плати за третьи коньки');
