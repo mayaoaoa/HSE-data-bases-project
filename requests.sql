b0ae0c0b0402530a51115d91089d1684a9ad121a
maya <maiia.ovsiannikova@gmail.com>
2023-12-21 17:36:16 +0300

    task 7

--- requests.sql
+++ requests.sql
@@ -452,4 +452,32 @@ select c.coach_name                                            as coach,
        c.experience                                            as XP,
        overlay(c.phone::text placing '-***-***-' from 2 for 6) as phone
 from rental_service.coach c
-order by XP desc ;
+order by XP desc;
+
+-- services
+drop view if exists services;
+create view services as
+select s.naming,
+       s.price
+from rental_service.service s
+order by s.price desc;
+
+-- addresses
+drop view if exists addresses;
+create view addresses as
+select a.street || ', дом ' || a.house                         as address,
+       overlay(a.phone::text placing '-***-***-' from 2 for 6) as phone
+from rental_service.address a
+order by address desc;
+
+
+-- promotions
+drop view if exists promotions;
+create view promotions as
+select p.promotion_name    as naming,
+       promotion_type_id   as type,
+       p.begin_t           as starts,
+       p.end_t             as ends,
+       p.end_t - p.begin_t as duration
+from rental_service.promotion p
+order by p.begin_t;
