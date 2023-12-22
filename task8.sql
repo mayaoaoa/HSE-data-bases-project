ffd511ba7dd83921aae16e460cc2b3a64ce63e7c
LizaGrankina <liza.grankina@gmail.com>
2023-12-22 18:45:36 +0300

    task 8, 2 views

--- task8.sql
+++ task8.sql
@@ -1,5 +1,32 @@
 --task 8
-set search_path = rental_service_view;
+set
+search_path = rental_service_view;
+-- вывести общее число денег, сэкономленных каждым клиентом при использовании акций
+-- отсортировать от клиента с самым большим накоплением до самого маленького
+drop view if exists client_saving;
+create view client_saving as
+select cl.name || ' ' || cl.surname as client,
+       sum(case
+               when o.promotion_id is null
+                   then 0
+               when
+                   o.promotion_id = 1 or o.promotion_id = 2 or o.promotion_id = 7
+                   then s.price
+               when
+                   o.promotion_id = 3 or o.promotion_id = 6
+                   then s.price * 0.1
+               when o.promotion_id = 4
+                   then s.price * 0.2
+               when o.promotion_id = 5
+                   then s.price * 0.35
+           end)                     as total_saving
+from rental_service.client cl
+         inner join rental_service.occasion o on cl.client_id = o.client_id
+         inner join rental_service.service s on o.service_id = s.service_id
+         inner join rental_service.promotion p on p.promotion_id = o.promotion_id
+group by cl.name || ' ' || cl.surname
+order by total_saving desc;
+
 -- вывести прибыль по каждому адресу,
 -- отсортировав от самого прибыльного адреса, к самому неприбыльному
 drop view if exists address_profit;
@@ -27,11 +54,31 @@ from rental_service.address a
 group by full_address
 order by total_address_profit desc;
 
+-- вывести количество затрат каждого клиента на услуги любого тренера
+-- отсортировать от клиента с самыми большими затратами на тренера к самым маленьким
+drop view if exists client_spent_coach;
+create view client_spent_coach as
+select cl.name || ' ' || cl.surname as client,
+       ch.coach_name                as coach_name,
+       sum(case
+               when s.service_id >= 8 and o.promotion_id = 5
+                   then s.price * 0.65
+               when s.service_id >= 8
+                   then s.price
+           end)                     as spent_coach
+from rental_service.client cl
+         inner join rental_service.occasion o on cl.client_id = o.client_id
+         inner join rental_service.service s on o.service_id = s.service_id
+         inner join rental_service.service_x_coach sc on s.service_id = sc.service_id
+         inner join rental_service.coach ch on sc.coach_id = ch.coach_id
+group by cl.name || ' ' || cl.surname, ch.coach_name
+order by spent_coach desc;
+
 -- вывести прибыль, которую принесла каждая услуга
 -- отсортировать от самой прибыльной услуги, к самой неприбыльной
 drop view if exists service_profit;
 create view service_profit as
-select naming as service_name,
+select naming   as service_name,
        sum(case
                when
                    o.promotion_id = 1 or o.promotion_id = 2 or o.promotion_id = 7
@@ -45,7 +92,7 @@ select naming as service_name,
                    then s.price * 0.65
                when o.promotion_id is null
                    then s.price
-           end)                      as total_service_profit
+           end) as total_service_profit
 from rental_service.service s
          inner join rental_service.occasion o on o.service_id = s.service_id
          left join rental_service.promotion p on o.promotion_id = p.promotion_id
